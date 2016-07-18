20a2d68ed1464e5f1310b1ec189c0419f09d0323
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-07-18 20:54:05 +0000

    finish MVP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require toastr
 //= require jquery_ujs
 //= require materialize-sprockets
 //= require turbolinks
@@ -23,3 +24,23 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+	
+	
+	 toastr.options = {
+	                  "closeButton": false,
+	                  "debug": false,
+	                  "positionClass": "toast-bottom-right",
+	                  "onclick": null,
+	                  "showDuration": "300",
+	                  "hideDuration": "1000",
+	                  "timeOut": "5000",
+	                  "extendedTimeOut": "1000",
+	                  "showEasing": "swing",
+	                  "hideEasing": "linear",
+	                  "showMethod": "fadeIn",
+	                  "hideMethod": "fadeOut"
+	              }
+	
+	});
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'pages#home'
+  root 'jobs#index'
   get 'about', to: 'pages#about'
   
   resources :jobs
